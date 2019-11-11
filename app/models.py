5862c199559905fa1101b933876f9c76240b56ff
Base Camp <basecamp@ssa-406.local>
2019-11-11 12:47:47 -0600

    Added three models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.TextField()
+    rating = models.IntegerField()
+    genre = models.TextField()
+    runtime = models.TextField()
+
+class Showing(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    showtime = models.TextField()
+
+class Ticket(models.Model):
+    name = models.TextField()
+    purchased_at = models.DateTimeField()
+    showing = models.ForeignKey(Showing, on_delete=models.CASCADE)
+    
+
